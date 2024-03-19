85d067605d0346762eba45780fb426786f432b95
Michael Girikallo <michael.girikallo@gmail.com>
2024-03-19 22:24:20 +1100

    Completed contacts and clean up

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer(){
     return(
-        <div className="h-40" id="contact">
+        <div className="h-40 pt-12 bg-pearl dark:bg-gray" id="contact">
             Contacts
         </div>
     );
--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Projects() {
   const projects: {
     name: string;
@@ -10,7 +12,8 @@ export default function Projects() {
       name: "EZ Invoice",
       description: "Description of Project1",
       img: "/images/ezinvoice.jpg",
-      linkDemo: "https://se2021-23t1-einvoicing-frontend-f14a-di-git-342b0e-merryrosalie.vercel.app/",
+      linkDemo:
+        "https://se2021-23t1-einvoicing-frontend-f14a-di-git-342b0e-merryrosalie.vercel.app/",
       linkCode: "https://github.com/HARI-PRMD/SENG2021-application",
     },
     {
@@ -21,27 +24,43 @@ export default function Projects() {
       linkCode: "https://github.com/csesoc/trainee-3-23t2",
     },
     {
-        name: "Note Trader",
-        description: "Description of Project2",
-        img: "/images/notetrader.jpg",
-        linkDemo: "https://trainee-3-23t1-deploy.vercel.app/",
-        linkCode: "https://github.com/csesoc/trainee-3-23t1",
-      },
+      name: "Note Trader",
+      description: "Description of Project2",
+      img: "/images/notetrader.jpg",
+      linkDemo: "https://trainee-3-23t1-deploy.vercel.app/",
+      linkCode: "https://github.com/csesoc/trainee-3-23t1",
+    },
   ];
 
   return (
-    <div className="grid md:grid-cols-3" id="projects">
-      {projects.map((projects, i) => (
-        <div key={i} className="project">
+    <div className="grid md:grid-cols-3 py-12 justify-items-center" id="projects">
+      {projects.map((projects, index) => (
+        <div key={index} className="project ">
           <h3>{projects.name}</h3>
           <p>{projects.description}</p>
-          <img src={projects.img} alt={projects.name} />
-          <a href={projects.linkDemo} target="_blank" rel="noopener noreferrer">
-            Demo
-          </a>
-          <a href={projects.linkCode} target="_blank" rel="noopener noreferrer">
-            Code
-          </a>
+          <Image
+            src={projects.img}
+            alt={projects.name}
+            width={500}
+            height={500}
+            className="w-80 rounded-sm border-1 border-black"
+          />
+          <div>
+            <a
+              href={projects.linkDemo}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Demo
+            </a>
+            <a
+              href={projects.linkCode}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Code
+            </a>
+          </div>
         </div>
       ))}
     </div>
